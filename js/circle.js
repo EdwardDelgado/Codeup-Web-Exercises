b0d4f8f8a4746f40e751bd4f634b20c8a5296e9b
e2 <edward.delgado@icloud.com>
2017-08-23 15:51:37 -0500

    Complete Objects Exercise

--- js/circle.js
+++ js/circle.js
@@ -5,23 +5,30 @@
     var circle = {
         radius: 3,
 
-        getArea: function circle(radius) {
+        getArea: function () {
             // TODO: complete this method
             // hint: area = pi * radius^2
 
-            circle.area = pi * radius^2
+        var area = Math.PI * Math.pow(this.radius, 2);
 
+        return area; // TODO: return the proper value
 
-            return; // TODO: return the proper value
         },
 
         logInfo: function (doRounding) {
+            var area;
+
+            if (doRounding) {
+                area = Math.round(this.getArea())
+            }
             // TODO: complete this method.
 
             // If doRounding is true, round the result to the nearest integer.
             // Otherwise, output the complete value
 
-            console.log("Area of a circle with radius: " + this.radius + ", is: ");
+            console.log(
+                "Area of a circle with radius: " + this.radius + ", is: " + area
+            );
         }
     };
 
@@ -33,6 +40,8 @@
 
     console.log("=======================================================");
     // TODO: Change the radius of the circle to 5.
+
+    circle.radius = 5;
 
     // log info about the circle
     console.log("Raw circle information");
