a1cb9a8bb040291f6ade7b6dd3c89b660892a5c7
e2 <edward.delgado@icloud.com>
2017-08-23 11:24:42 -0500

    Complete Objects Exercise

--- js/objects.js
+++ js/objects.js
@@ -2,14 +2,14 @@
     "use strict";
 
     /**
-     * TODO:
+     * :
      * Create person object, store it in a variable named person
      */
 
     var person = {};
 
     /**
-     * TODO:
+     * :
      * Create firstName and lastName properties in your person object, and
      * assign your name to them
      */
@@ -18,7 +18,7 @@
     person.lastName = "Delgado";
 
     /**
-     * TODO:
+     * :
      * Add a sayHello method to the person object that returns a greeting using
      * the firstName and lastName properties.
      * console.log the returned message to check your work
@@ -33,7 +33,7 @@
 
     console.log(person.sayHello());
 
-    /** TODO: Remember this problem from before?
+    /** : Remember this problem from before?
      *
      * HEB has an offer for the shoppers that buy products amounting to more
      * than $200. Write a JS program, using conditionals, that logs to the
@@ -53,11 +53,74 @@
          {name: 'George', amount: 320}
      ];
 
-    var discount = 0;
-    var totalAfterDiscount = shoppers[0].amount;
+    for (var i = 0; i < shoppers.length; i++) {
+    var shopper = shoppers[i];
+    console.log(buildReceipt(shopper));
+    }
+
+    shoppers.forEach(function (shopper) {
+        console.log(buildReceipt(shopper));
+    });
+
+    function buildReceipt(shopper) {
+        var discount = 0;
+        var totalAfterDiscount = shopper.amount;
 
 
+        // 2. Conditionals
+        if (shopper.amount > 200) {
+            discount = .1;
+            totalAfterDiscount = shopper.amount - shopper.amount * discount;
+        }
+        return shopper.name
+            + " has to pay $" + shopper.amount
+            + ", it has a discount of $" + discount
+            + ", the total to pay after the discount is $" + totalAfterDiscount;
+    }
 
-    console.log("Cameron has to pay $180, it has a discount of $0, the totla to pay after the discount is $0");
 
+    "use strict";
+
+// todo:
+// Create an array of objects that represent books.
+// Each book should have a title and an author property.
+// The author property should be an object with a firstName and lastName.
+// Be creative and add at least 5 books to the array
+    var books = [
+        {
+            title: "97 Things Every Programmer Should Know",
+            author: {firstName: "Kevlin", lastName: "Henney"}
+        },
+        {
+            title: "Growing Object-Oriented Software Guided by Tests",
+            author: {firstName: "Steve", lastName: "Freeman"}
+        },
+        {
+            title: " Working Effectively with Legacy Code",
+            author: {firstName: "Michael", lastName:  "Feathers"}
+        },
+        {
+            title: "Refactoring",
+            author: {firstName: "Martin", lastName: "Fowler"}
+        },
+        {
+            title: "The Pragmatic Programmer",
+            author: { firstName: "Andy", lastName: "Hunt"}
+        }
+    ];
+
+// log out the books array
+    console.log(books);
+
+// todo:
+// Loop through the array of books using .forEach and print out the specified information about each one.
+// start loop here
+    books.forEach(function (book, i) {
+
+        console.log("Book #" + (i + 1));
+        console.log("Title: " + book.title);
+        console.log("Author: " + book.author.firstName + " " + book.author.lastName);
+        console.log("---");
+    });
+// end loop here
 })();
