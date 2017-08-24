618bf9ec75efd2a5a952d866a351bac1a191252f
e2 <edward.delgado@icloud.com>
2017-08-23 20:21:02 -0500

    Complete external/inline JS Exercises

--- js/external.js
+++ js/external.js
@@ -1,2 +1,9 @@
-"use strict";
-console.log ("Hello from external JavaScript");
+'use strict';
+
+console.log('Hello from external js!');
+
+alert('Welcome to my website!');
+
+var usersFavoriteColor = prompt('What is your favorite color?');
+
+alert('Yay!, ' + usersFavoriteColor + ' is my favorite too!');
