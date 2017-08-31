8865ef4234f9c68242ab0ec6cfece5ad1b0ed8a9
e2 <edward.delgado@icloud.com>
2017-08-31 16:22:54 -0500

    Completed  jQuery Exercise

--- js/jquery_exercises.js
+++ js/jquery_exercises.js
@@ -1,14 +1,23 @@
 "use strict"
 
-$('.codeup').css('background-color', 'yellow');
-$('.codeup').css('border', '1px solid red');
+$('.codeup').click(function() {
+    $(".codeup").css("background-color", "blue");
+});
 
-(function () {
-    $("li").css("font-size", "20px");
-    $("h1, p, li").css("background-color", "yellow");
-        ($("h1").text());
-        ($("h1").html());
-    }
-)
+$('p').dblclick(function() {
+    $("p").css("font-size", "18px");
+});
+
+$("li").hover(
+    function() {
+        // first change
+        $(this).css("background-color", "red");
+
+});
+
+
+
+
+
 
 
