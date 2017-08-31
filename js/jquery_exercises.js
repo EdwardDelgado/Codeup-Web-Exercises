1195936c560b5531e29e12f4d57656d6391f30c7
e2 <edward.delgado@icloud.com>
2017-08-31 13:47:02 -0500

    Completed  jQuery Exercise

---
+++ js/jquery_exercises.js
@@ -0,0 +1,14 @@
+"use strict"
+
+$('.codeup').css('background-color', 'yellow');
+$('.codeup').css('border', '1px solid red');
+
+(function () {
+    $("li").css("font-size", "20px");
+    $("h1, p, li").css("background-color", "yellow");
+        ($("h1").text());
+        ($("h1").html());
+    }
+)
+
+
